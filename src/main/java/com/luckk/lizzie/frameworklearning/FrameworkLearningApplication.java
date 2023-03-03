68da95355f0709157e98ea10c8a80a991aee3956
luckkun <1546165200@qq.com>
2023-03-04 01:00:36 +0800

    基础库

---
+++ src/main/java/com/luckk/lizzie/frameworklearning/FrameworkLearningApplication.java
@@ -0,0 +1,13 @@
+package com.luckk.lizzie.frameworklearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luckk/lizzie/frameworklearning/FrameworkLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckk.lizzie.frameworklearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
